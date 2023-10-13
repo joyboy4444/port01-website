9941278948897c45fddfd4bb4c263cc0b82bc9ad
joyboy4444 <144307879+joyboy4444@users.noreply.github.com>
2023-10-13 21:15:44 +0700

    port01

---
+++ app.js
@@ -0,0 +1,16 @@
+const sr = ScrollReveal ({
+    distance: '45px',
+    duration:2400,
+    reset:true
+});
+
+sr.reveal('.mar-img',{delay:150,origin:'left'});
+sr.reveal('.main-text h1',{delay:250,origin:'top'});
+sr.reveal('.main-text h4',{delay:400,origin:'right'});
+sr.reveal('.social ',{delay:150,origin:'buttom'});
+
+sr.reveal('.row ',{delay:510,origin:'left'});
+sr.reveal('.button ',{delay:600,origin:'top'});
+sr.reveal('.center ',{delay:700,origin:'right'});
+
+sr.reveal('.portfolio ',{delay:800,origin:'top'});
